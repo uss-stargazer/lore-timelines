6cbf8c11bdc2bafe32fe02c2fd1031ef9c2687a8
anonymous <anonymous>
2025-10-11 23:44:50 -0400

    Remove elements rendered unnecessary by styled/text elements

--- src/pages/Realms/pages/Earth/index.tsx
+++ src/pages/Realms/pages/Earth/index.tsx
@@ -2,12 +2,11 @@ import { Box } from "@mui/material";
 import { Route } from "react-router-dom";
 
 import bannerImage from "./assets/images/banner.jpg";
-import RealmBanner from "../../components/RealmBanner";
 
 function Earth() {
   return (
     <Box>
-      <RealmBanner
+      {/* <RealmBanner
         realm="Earth"
         description={`Overview of Earth's lore (also known as world history). This timeline 
                       aggregates interesting topics that I've researched through Afro-Eurasia 
@@ -17,14 +16,11 @@ function Earth() {
           description: `A world map part of what's considered the first true atlas (Theatrum 
                         Orbis Terrarum by Abraham Orteliush).`,
         }}
-      />
+      /> */}
     </Box>
   );
 }
 
-export const EarthRoutes = (
-  <Route path="earth" element={<Earth />}>
-  </Route>
-);
+export const EarthRoutes = <Route path="earth" element={<Earth />}></Route>;
 
 export default Earth;
--- src/pages/Realms/pages/StarTrek/index.tsx
+++ src/pages/Realms/pages/StarTrek/index.tsx
@@ -2,19 +2,18 @@ import { Box } from "@mui/material";
 import { Route } from "react-router-dom";
 
 import bannerImage from "./assets/images/banner.jpg";
-import RealmBanner from "../../components/RealmBanner";
 
 function StarTrek() {
   return (
     <Box>
-      <RealmBanner
+      {/* <RealmBanner
         realm="Star Trek"
         description={`Star Trek realm description.`}
         image={{
           path: bannerImage,
           description: "USS Enterprise (NCC-1701-D) from The Next Generation",
         }}
-      />
+      /> */}
     </Box>
   );
 }
